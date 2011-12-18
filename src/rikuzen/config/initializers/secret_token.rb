dd94d1f2c7f8dca4894e4213e8641a45b839a405
KOMATSU Issei <issei@mizuhataya.com>
2011-12-19 00:17:04 +0900

    rails テンプレート作成

---
+++ src/rikuzen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rikuzen::Application.config.secret_token = '36ef184e68a85f9eaee5482577a5e42c3d9f69924b5d2f9dcf7861cdc4815654ed04b9a4dae406e7177e32a869fc35eb16aa03dd3cadafabdf446bbeb08b9ca1'
